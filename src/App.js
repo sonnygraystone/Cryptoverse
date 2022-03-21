0cba25af45d4a59cdd80c1d88a3bf5c706ced719
sonnygraystone <sonny.nguyen26@gmail.com>
2022-03-21 14:29:18 +1300

    Added navigation links & components
    Added navigation links & components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import { Layout, Typograpgy, Space } from "antd";
+import { Routes, Route, Link } from "react-router-dom";
 
-import { NavBar } from "./components/";
+import { Layout, Typography, Space } from "antd";
+
+import Exchanges from "./components/Exchanges";
+import Homepage from "./components/Homepage";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+
 import "./App.css";
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="navbar"></div>
+const App = () => (
+  <div className="app">
+    <div className="NavBar">
       <NavBar />
-      <div className="main"></div>
-      <div className="footer"></div>
     </div>
-  );
-};
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/exchanges/" element={<Exchanges />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="//crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/News" element={<News />} />
+          </Routes>
+        </div>
+      </Layout>
+    </div>
+  </div>
+);
 
 export default App;
